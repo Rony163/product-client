4028aaeee5c1bfc0d49593cfb710888a467e53ee
Rony163 <minhajulislamrony43@gmail.com>
2021-10-24 03:53:59 +0600

    products data load from server

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div>
-            <h1>This is products</h1>
+            <h1>Products available: {products.length}</h1>
+            <ul>
+                {
+                    products.map(product => <li key={product._id}>
+                        Name:: {product.name} , Price:: {product.price} ,Quantity:: {product.quantity}
+                        <button>Update</button>
+                        <button>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
